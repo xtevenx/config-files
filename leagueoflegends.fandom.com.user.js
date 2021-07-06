d0d1ad37b72afb3ec814d95125be458968014966
Steven Xia <33334736+xtevenx@users.noreply.github.com>
2021-07-06 15:38:53 -0700

    Add new userscript.

---
+++ leagueoflegends.fandom.com.user.js
@@ -0,0 +1,28 @@
+// ==UserStyle==
+// @name         LoL Wiki Clean
+// @description  Clean style for the League of Legends Fandom Wiki.
+// @version      0.0.0-dev
+// @author       xtevenx
+// @namespace    xtevenx
+// @license      GPL-3.0-or-later
+// @match        https://leagueoflegends.fandom.com/*
+// ==/UserStyle==
+
+// The locations of the elements are in parentheses after they are described.
+// These locations are relative to the main page information.
+
+// Remove the global navigation bar (left).
+for (const e in document.getElementsByClassName("global-navigation")) {
+    e.remove();
+}
+
+// Remove the comments section (bottom).
+document.getElementById("mw-data-after-content").remove();
+
+// Remove the random links section (bottom).
+document.getElementById("mixed-content-footer").remove();
+
+// Remove the global page footer (bottom).
+for (const e in document.getElementsByClassName("wds-global-footer")) {
+    e.remove();
+}
