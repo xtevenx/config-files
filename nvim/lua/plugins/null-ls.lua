01c72e34dda755e3d32ea8a658440500329854f9
Steven Xia <stevenxia@protonmail.com>
2022-11-30 13:47:12 -0800

    [nvim] Prefer venv versions of python linters

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -7,10 +7,14 @@ null_ls.setup {
     null_ls.builtins.diagnostics.cppcheck,
     -- https://github.com/PyCQA/flake8
     -- Run `pip install flake8`
-    null_ls.builtins.diagnostics.flake8,
+    null_ls.builtins.diagnostics.flake8.with {
+      prefer_local = ".venv/bin",
+    },
     -- https://github.com/PyCQA/pylint
     -- Run `pip install pylint`
-    null_ls.builtins.diagnostics.pylint,
+    null_ls.builtins.diagnostics.pylint.with {
+      prefer_local = ".venv/bin",
+    },
     -- https://www.kernel.org/doc/html/latest/process/clang-format.html
     -- Comes with `pacman -S clang`
     -- <ELSE> Run `npm install -g clang-format`
@@ -19,9 +23,13 @@ null_ls.setup {
     },
     -- https://github.com/PyCQA/isort
     -- Run `pip install isort`
-    null_ls.builtins.formatting.isort,
+    null_ls.builtins.formatting.isort.with {
+      prefer_local = ".venv/bin",
+    },
     -- https://github.com/google/yapf
     -- Run `pip install yapf`
-    null_ls.builtins.formatting.yapf,
+    null_ls.builtins.formatting.yapf.with {
+      prefer_local = ".venv/bin",
+    },
   },
 }
